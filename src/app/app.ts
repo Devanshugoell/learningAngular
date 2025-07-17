f3ddb6c5b37c0c21192e5e7ce5216cc03859d921
Devanshu <devgoyal952@gmail.com>
2025-07-17 11:30:01 +0530

    Added routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, effect, Signal, signal, WritableSignal , computed } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { Header } from './header/header';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,                 
-  imports: [RouterOutlet, ButtonModule,FormsModule], 
+  imports: [RouterOutlet,Header], 
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
@@ -14,14 +14,4 @@ import { ButtonModule } from 'primeng/button';
 
 export class App {
 
-zoom = false;
-
-BigHeading = "100px";
-SmallHeading = "10px";
-
-updateHeadingSize() {
-  this.zoom = !this.zoom;
-}
-
-Bigger = "50px";
 }
