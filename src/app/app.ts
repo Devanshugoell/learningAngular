5305f8e6391c659f7026ed1651083e4ad9977219
Devanshu <devgoyal952@gmail.com>
2025-07-18 15:42:32 +0530

    removed Code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { Product } from "./services/product";
 
 @Component({
   selector: "app-root",
@@ -8,20 +7,4 @@ import { Product } from "./services/product";
   templateUrl: "./app.html",
   styleUrls: ["./app.css"],
 })
-export class App {
-  productData:
-    | {
-        name: string;
-        price: number;
-      }[]
-    | undefined;
-
-  constructor(private Product: Product) {
-    console.log("App component initialized");
-  }
-
-  getProductData() {
-    this.productData = this.Product.getProductData();
-    console.log("Product data loaded:", this.productData);
-  }
-}
+export class App {}
