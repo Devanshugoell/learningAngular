0e8f3917f2c32b34c21783ef5876dc3d86a519cc
Devanshu <devgoyal952@gmail.com>
2025-07-18 17:01:22 +0530

    How to call api in services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Products } from "./services/products";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,13 @@ import { Component } from "@angular/core";
   templateUrl: "./app.html",
   styleUrls: ["./app.css"],
 })
-export class App {}
+export class App {
+  productList: any;
+  constructor(private Products: Products) {}
+
+  ngOnInit() {
+    this.Products.getProductList().subscribe((data) => {
+      this.productList = data;
+    });
+  }
+}
